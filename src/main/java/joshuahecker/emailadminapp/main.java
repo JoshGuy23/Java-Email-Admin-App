3b12002a27139348bd6b6a10009ed653dd477deb
joshua <jhecker2001@gmail.com>
2024-04-29 09:17:26 -0500

    Added Scanner, added functionality to get the user's first and last name.

---
+++ src/main/java/joshuahecker/emailadminapp/main.java
@@ -0,0 +1,15 @@
+package joshuahecker.emailadminapp;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Please enter your first name.");
+        String f_name = input.nextLine().replaceAll("\\s", "");
+        
+        System.out.println("Please enter your first name.");
+        String l_name = input.nextLine().replaceAll("\\s", "");
+    }
+    
+}
